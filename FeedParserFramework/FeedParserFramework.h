0367a437b2ea5d01c355fea82c49f734a165208c
Benedikt Terhechte <terhechte@gmail.com>
2016-06-21 11:43:06 +0300

    Added Framework

---
+++ FeedParserFramework/FeedParserFramework.h
@@ -0,0 +1,19 @@
+//
+//  FeedParserFramework.h
+//  FeedParserFramework
+//
+//  Created by Benedikt Terhechte on 20/06/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FeedParserFramework.
+FOUNDATION_EXPORT double FeedParserFrameworkVersionNumber;
+
+//! Project version string for FeedParserFramework.
+FOUNDATION_EXPORT const unsigned char FeedParserFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedParserFramework/PublicHeader.h>
+
+
